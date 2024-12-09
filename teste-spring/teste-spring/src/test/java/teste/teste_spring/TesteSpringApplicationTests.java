12dd7c67811e9b67c32a1a815b632738f1a32ed4
devcelsoborges <borgesnetocs@gmail.com>
2024-12-09 20:56:44 -0300

    Upload projeto springboot de teste

---
+++ teste-spring/teste-spring/src/test/java/teste/teste_spring/TesteSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package teste.teste_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
